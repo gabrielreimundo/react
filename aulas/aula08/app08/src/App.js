9cba61d53539defd8c029fb40f2a1b3891a1de5b
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2023-04-07 00:11:59 -0300

    at

--- aulas/aula08/app08/src/App.js
+++ aulas/aula08/app08/src/App.js
@@ -2,18 +2,11 @@ import React from 'react';
 
 export default function App(){
 
-  const carros=[
-    {categoria: "Esporte", preco: "1100000.00", modelo: "Golf GTI"},
-    {categoria: "Esporte", preco: "1200000.00", modelo: "Camaro"},
-    {categoria: "SUV", preco: "85000.00", modelo: "HRV"},
-    {categoria: "SUV", preco: "83000.00", modelo: "T-Cross"},
-    {categoria: "Ultilitario", preco: "120000.00",modelo: "Hillux"},
-    {categoria: "Ultilitario", preco: "90000.00", modelo: "Ranger"}
-  ];
+  const carros=["Carro1","Carro2","Carro3"];
 
   const listaCarros=carros.map(
     (c,i)=>
-      <li key={i}>{i} - {c.categoria} {c.modelo}- R${c.preco}</li>
+      <li key={i}>{i} - {c}</li>
   )
 
   return (
