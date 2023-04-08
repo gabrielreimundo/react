4f2e2b08feb6a89044467e1f27f855a9ca3b0e3d
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2023-04-07 22:36:39 -0300

    at

--- aulas/aula11/app11/src/App.js
+++ aulas/aula11/app11/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
 
-function App() {
+export default function App(){
+  
+  const [nota1,setNota1]=useState(0)
+  const [nota2,setNota2]=useState(0)
+  const [nota3,setNota3]=useState(0)
+  const [nota4,setNota4]=useState(0)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+    </>
   );
 }
-
-export default App;
---
+++ aulas/aula11/app11/src/componentes/Nota.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Nota(props){
+    return(
+        <div>
+            <legend>Informe a nota: </legend>
+            <input type='text' value={} onChange={}></input>
+        </div>
+    )
+}
