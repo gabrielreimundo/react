7931e5d1d5e6bd4293ccbe6ed490262c34f1d0bd
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2023-04-16 01:50:01 -0300

    at

--- aulas/aula20/app20/src/App.js
+++ aulas/aula20/app20/src/App.js
@@ -6,14 +6,35 @@ export default  function App() {
 
   const [pagina,setPagina]=useState(0)
 
+  useEffect(
+    ()=>{
+      const url=window.location.href
+      const res=url.split('?')
+      setPagina(parseInt(res[2]))
+    }
+  )
+
+  const LinksPaginas=(p)=>{
+    if(p==1){
+      window.open('http://localhost:3001/?1','_self')
+    }else if(p==2){
+      window.open('http://localhost:3001/?2','_self')
+    }
+  }
+
   const retornarPagina=()=>{
     if(pagina==1){
       return <Pagina1/>
     }else if(pagina==2){
       return <Pagina2/>
     }else{
-      return
-    }
+      return(
+        <div>
+          <button onClick={()=>{LinksPaginas(1)}}>Pagiona1</button>
+          <button onClick={()=>{LinksPaginas(2)}}>Pagina2</button>
+        </div>
+       )
+     }
   }
 
   return (
