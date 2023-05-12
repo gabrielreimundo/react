5ba8b0ff85c7476d4c27f22e87d813a089bbd3a6
Gabriel Reimundo <109987851+gabrielreimundo@users.noreply.github.com>
2023-05-12 14:42:01 -0300

    at

---
+++ aulas/aula25/app25/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ListaCarros from './componentes/listaCarros';
+
+// https://www.youtube.com/watch?v=GrxM7aHyN84&list=PLx4x_zx8csUh752BVDGZkxYpY9lS40fyC&index=39
+
+export default function App() {
+  return (
+    <>
+      <ListaCarros/>
+    </>
+  );
+}
